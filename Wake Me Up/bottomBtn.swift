c388a307d2c9ead72ac3e962616c0f1e7f488351
Richard Chiang <shadowlairs@gmail.com>
2017-11-08 23:38:39 -0800

    Add new button styles

---
+++ Wake Me Up/bottomBtn.swift
@@ -0,0 +1,22 @@
+//
+//  bottomBtn.swift
+//  Wake Me Up
+//
+//  Created by Richard Chiang on 2017-11-08.
+//  Copyright © 2017 Richard Chiang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class bottomBtn: UIButton {
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.borderWidth = 1.0
+        layer.borderColor = UIColor.black.cgColor
+        layer.masksToBounds = true
+        layer.cornerRadius = 10
+        layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+    }
+}
