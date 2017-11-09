bf3c6d950ece84f6e8aa432face35623222e7513
Richard Chiang <shadowlairs@gmail.com>
2017-11-09 13:01:32 -0800

    Add Alarm class

---
+++ Wake Me Up/ALarm.swift
@@ -0,0 +1,31 @@
+//
+//  Alarm.swift
+//  Wake Me Up
+//
+//  Created by Richard Chiang on 2017-11-09.
+//  Copyright © 2017 Richard Chiang. All rights reserved.
+//
+
+import Foundation
+
+class Alarm {
+    
+    var hour = 0
+    var minute = 0
+    
+    required init(atHour h: Int, minute m: Int) {
+        self.hour = h
+        self.minute = m
+    }
+    
+    func isItNow() -> Bool {
+        let date = Date()
+        let calendar = Calendar.current
+        
+        let hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+        
+        return hour == self.hour && minute == self.minute
+    }
+    
+}
